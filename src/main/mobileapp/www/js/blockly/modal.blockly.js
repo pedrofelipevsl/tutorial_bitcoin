5e2b2ce3acdb0f34653018ade320c7ed737c3be2
Pedro Lima <pedro.lima@cronapp.io>
2020-01-02 11:47:41 +0000

    Commit 1 projeto já em andamento

---
+++ src/main/mobileapp/www/js/blockly/modal.blockly.js
@@ -0,0 +1,12 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Modal = window.blockly.js.blockly.Modal || {};
+
+/**
+ * modal
+ */
+window.blockly.js.blockly.Modal.AbrirModal = function() {
+
+  this.cronapi.screen.showIonicModal("modal65317");
+}
